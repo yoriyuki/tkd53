d2ea763586c15d4a2619393ce465ccdf9e036e04
hirokuni-maeta <hirokuni.maeta+github@gmail.com>
2016-03-14 19:53:40 +0900

    リクエストとレスポンス処理をリファクタ

--- src/server/unix_domain_socket.cpp
+++ src/server/unix_domain_socket.cpp
@@ -33,23 +33,18 @@ const string kUnixDomainPath = "/tmp/lime_sock";
 namespace {
 
 
-class SocketHolder {
+class SocketCloser {
 public:
-  SocketHolder(const int sock)
+  SocketCloser(const int sock)
     : sock_(sock) {
   }
 
-  ~SocketHolder() {
+  ~SocketCloser() {
     if (sock_ < 0) {
       return;
     }
     close(sock_);
   }
-
-  int get() const {
-    return sock_;
-  }
-
 private:
   const int sock_;
 };
@@ -61,50 +56,68 @@ void PerrorAndExit(string msg) {
 }
 
 
-void HandleClient(const SocketHolder &client_socket, Engine *engine) {
+bool ReadLine(const int socket, char* const read_buffer, const size_t size) {
+  size_t read_index = 0;
+  while ((read_index == 0) ||
+         (read_index < size && read_buffer[read_index-1] != '\n')) {
+    const int recieved_size =
+      recv(socket, &read_buffer[read_index], size - read_index, 0);
+    if (recieved_size < 0) {
+      return false;
+    }
+    read_index += static_cast<size_t>(recieved_size);
+  }
+  read_buffer[read_index - 1] = '\0';
+  return true;
+}
+
+
+bool WriteLine(const int socket,
+               const char* const write_buffer,
+               const size_t size) {
+  size_t bytes_send = 0;
+  while (bytes_send != size) {
+    const int bytes = send(socket, write_buffer + bytes_send, size, 0);
+    if (bytes < 0) {
+      return false;
+    }
+    bytes_send += static_cast<size_t>(bytes);
+  }
+  return true;
+}
+
+
+bool HandleClient(const int client_socket, Engine *engine) {
   char read_buffer[1024];
 
   while (1) {
-    int read_index = 0;
-
-    while (read_index == 0 || read_buffer[read_index-1] != '\n') {
-      // todo: buffer overlflow
-      int recieved_size = 0;
-      if ((recieved_size = recv(client_socket.get(),
-                                read_buffer + read_index,
-                                sizeof(read_buffer) - read_index,
-                                0)) < 0) {
-        PerrorAndExit("recv() failed.");
-      }
-
-      read_index += recieved_size;
+    if (!ReadLine(client_socket, read_buffer, sizeof(read_buffer))) {
+      PerrorAndExit("ReadLine() failed.");
     }
 
-    read_buffer[read_index - 1] = '\0';
+    cerr << "Request: (" << read_buffer << ")" << endl;
 
-    cout << "[" << read_buffer << "]" << endl;
-
-    string response_string;
-
+    string response_string = read_buffer;
     if (!HandleRequest(string(read_buffer), engine, &response_string)) {
-      continue;
+      break;
     }
 
-    cout << "done" << endl;
+    cerr << "Response: (" << response_string << ")" << endl;
 
-    if (send(client_socket.get(),
-             response_string.c_str(),
-             response_string.size(), 0) != int(response_string.size())) {
-      PerrorAndExit("send() failed.");
+    if (!WriteLine(client_socket,
+                   response_string.c_str(),
+                   response_string.size())) {
+      PerrorAndExit("WriteLine() failed.");
     }
   }
+  return true;
 }
 
 
 } // namespace
 
 int main(void) {
-  cout << "Starting Kkc server"  << endl;
+  cerr << "Starting Kkc server"  << endl;
 
   shared_ptr<Bigram> bigram(new Bigram());
   bigram->Init(ifstream(kNgramFreqFile, ios::in));
@@ -115,14 +128,15 @@ int main(void) {
   Engine engine(shared_ptr<ConverterInterface>(
       new BigramConverter(bigram, dict)));
 
-  SocketHolder server_socket(socket(AF_UNIX, SOCK_STREAM, 0));
-
-  if ((server_socket.get()) < 0) {
+  int server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+  if ((server_socket) < 0) {
     PerrorAndExit("faild to open socket\n");
   }
 
+  SocketCloser server_socket_closer(server_socket);
+
   int reuse_addr = 1;
-  if ((setsockopt(server_socket.get(),
+  if ((setsockopt(server_socket,
                   SOL_SOCKET,
                   SO_REUSEADDR,
                   (const void *)&reuse_addr,
@@ -136,25 +150,27 @@ int main(void) {
 	server_addr.sun_family = AF_UNIX;
 	strcpy(server_addr.sun_path, "/tmp/lime_sock");
 
-  if (bind(server_socket.get(),
+  if (bind(server_socket,
            (struct sockaddr *) &server_addr,
            sizeof(server_addr)) < 0) {
     PerrorAndExit("bind() failed.");
   }
 
-  if (listen(server_socket.get(), 5) < 0) {
+  if (listen(server_socket, 5) < 0) {
     PerrorAndExit("listen() failed.");
   }
 
   while (1) {
     struct sockaddr_in client_addr;
     socklen_t size = sizeof(client_addr);
-    SocketHolder client_socket(
-        accept(server_socket.get(), (struct sockaddr *) &client_addr, &size));
-
-    if (client_socket.get() < 0) {
+    int client_socket = accept(server_socket,
+                               (struct sockaddr *) &client_addr,
+                               &size);
+    if (client_socket < 0) {
       PerrorAndExit("accept() failed.");
     }
+
+    SocketCloser client_socket_closer(client_socket);
 
     cout << "Processing new connection" << endl;
 
