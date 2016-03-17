8cdaa282b2e443e20a8846b5c5606ff77d1f3e31
hmaeta <hirokuni.maeta+github@gmail.com>
2016-03-17 10:29:56 +0900

    EngineにListCandidateを定義

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -5,12 +5,23 @@
 namespace lime {
 namespace engine {
 
-Engine::Engine(shared_ptr<AbstractConverter> converter)
-  : converter_(converter) {
+Engine::Engine(shared_ptr<AbstractConverter> converter,
+               shared_ptr<DictionaryInterface> dictionary)
+  : converter_(converter), dictionary_(dictionary) {
 }
 
 void Engine::Convert(const KkciString &input, Segments *output) {
   converter_->Convert(input, kBOS, kBOS, output);
+}
+
+void Engine::ListCandidates(const KkciString &input,
+                            vector<const Entry*> *output) {
+  ScopedDictionaryResetter resetter(dictionary_);
+  for (KkciString::const_iterator it = input.begin();
+       it != input.end(); ++it) {
+    dictionary_->PushBack(*it);
+  }
+  dictionary_->Lookup(output);
 }
 
 } // converter
--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+#include "dictionary/dictionary_interface.hpp"
 #include "converter/abstract_converter.hpp"
 
 namespace lime {
@@ -10,16 +11,22 @@ using namespace std;
 using namespace lime::base::kkci;
 using namespace lime::base::token;
 using namespace lime::converter;
+using namespace lime::dictionary;
 
 
 class Engine {
 public:
-  Engine(shared_ptr<AbstractConverter> converter);
+  Engine(shared_ptr<AbstractConverter> converter,
+         shared_ptr<DictionaryInterface> dictionary);
 
   void Convert(const KkciString &input, Segments *output);
 
+  void ListCandidates(const KkciString &input, vector<const Entry*> *output);
+
 private:
   shared_ptr<AbstractConverter> converter_;
+
+  shared_ptr<DictionaryInterface> dictionary_;
 };
 
 
